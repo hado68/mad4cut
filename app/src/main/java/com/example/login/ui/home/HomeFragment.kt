4158895514e60091411827364ffa43465d1d1cee
Dohyun Ha <hdh6748@gmail.com>
2024-07-07 19:40:05 +0900

    Implement camera

--- app/src/main/java/com/example/login/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/login/ui/home/HomeFragment.kt
@@ -1,11 +1,33 @@
 package com.example.login.ui.home
 
+import android.Manifest
+import android.content.Context.CAMERA_SERVICE
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.graphics.ImageFormat
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
+import android.hardware.camera2.CameraMetadata
+import android.hardware.camera2.CaptureRequest
+import android.hardware.camera2.TotalCaptureResult
+import android.media.Image
+import android.media.ImageReader
 import android.os.Bundle
+import android.os.Environment
+import android.os.Handler
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Surface
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.login.RetrofitClient
 import com.example.login.databinding.FragmentHomeBinding
@@ -14,14 +36,24 @@ import com.example.login.models.NameResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
-    private val apiService: ApiService by lazy {
-        RetrofitClient.getClient(requireContext()).create(ApiService::class.java)
-    }
+    private lateinit var textureView: TextureView
+    private lateinit var handler: Handler
+    private lateinit var cameraDevice: CameraDevice
+    private lateinit var captureSession: CameraCaptureSession
+    private lateinit var captureRequestBuilder: CaptureRequest.Builder
+    private val REQUEST_CAMERA_PERMISSION = 100
+    private val REQUEST_WRITE_PERMISSION = 101
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -34,36 +66,207 @@ class HomeFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.tempButton.setOnClickListener{
-            fetchUserProfile()
+        handler = Handler()
 
+        textureView = binding.textureView
+        textureView.surfaceTextureListener = object : TextureView.SurfaceTextureListener {
+            override fun onSurfaceTextureAvailable(surface: SurfaceTexture, width: Int, height: Int) {
+                openCamera()
+            }
+
+            override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture, width: Int, height: Int) {}
+
+            override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {}
+
+            override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+                return true
+            }
+        }
+
+        binding.tempButton.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_PERMISSION)
+            } else {
+                if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_WRITE_PERMISSION)
+                } else {
+                    startCountdown()
+                }
+            }
         }
     }
+    private fun startCountdown() {
+        binding.countdownTextView.text = "5"
+        handler.postDelayed({ binding.countdownTextView.text = "4" }, 1000)
+        handler.postDelayed({ binding.countdownTextView.text = "3" }, 2000)
+        handler.postDelayed({ binding.countdownTextView.text = "2" }, 3000)
+        handler.postDelayed({ binding.countdownTextView.text = "1" }, 4000)
+        handler.postDelayed({ takePicture() }, 5000)
+    }
+    private fun openCamera() {
+        val cameraManager = requireActivity().getSystemService(CAMERA_SERVICE) as CameraManager
+        try {
+            val cameraId = cameraManager.cameraIdList[0]
+            val cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId)
+            val map = cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
+            val previewSize = map!!.getOutputSizes(SurfaceTexture::class.java)[0]
+
+            if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                return
+            }
+            cameraManager.openCamera(cameraId, object : CameraDevice.StateCallback() {
+                override fun onOpened(camera: CameraDevice) {
+                    cameraDevice = camera
+                    val texture = textureView.surfaceTexture!!
+                    texture.setDefaultBufferSize(previewSize.width, previewSize.height)
+                    val surface = Surface(texture)
+                    captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                    captureRequestBuilder.addTarget(surface)
+                    cameraDevice.createCaptureSession(listOf(surface), object : CameraCaptureSession.StateCallback() {
+                        override fun onConfigured(session: CameraCaptureSession) {
+                            captureSession = session
+                            captureSession.setRepeatingRequest(captureRequestBuilder.build(), null, handler)
+                        }
+
+                        override fun onConfigureFailed(session: CameraCaptureSession) {}
+                    }, handler)
+                }
+
+                override fun onDisconnected(camera: CameraDevice) {
+                    camera.close()
+                }
+
+                override fun onError(camera: CameraDevice, error: Int) {
+                    camera.close()
+                }
+            }, null)
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+    }
+    private fun takePicture() {
+        if (cameraDevice == null) return
+
+        val cameraManager = requireActivity().getSystemService(CAMERA_SERVICE) as CameraManager
+        try {
+            val cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraDevice.id)
+            val jpegSizes = cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)!!.getOutputSizes(ImageFormat.JPEG)
+            val width = jpegSizes[0].width
+            val height = jpegSizes[0].height
+
+            val reader = ImageReader.newInstance(width, height, ImageFormat.JPEG, 1)
+            val outputSurfaces = ArrayList<Surface>(2)
+            outputSurfaces.add(reader.surface)
+            outputSurfaces.add(Surface(textureView.surfaceTexture))
+
+            val captureBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE)
+            captureBuilder.addTarget(reader.surface)
+            captureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO)
+
+            val file = File(Environment.getExternalStorageDirectory().toString() + "/" + Environment.DIRECTORY_PICTURES + "/pic.jpg")
+            val readerListener = ImageReader.OnImageAvailableListener { reader ->
+                var image: Image? = null
+                try {
+                    image = reader.acquireLatestImage()
+                    val buffer = image.planes[0].buffer
+                    val bytes = ByteArray(buffer.capacity())
+                    buffer.get(bytes)
+                    save(bytes, file)
+                    val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                    activity?.runOnUiThread {
+                        binding.capturedImage.setImageBitmap(bitmap)
+                        binding.capturedImage.visibility = View.VISIBLE
+                        binding.countdownTextView.visibility = View.GONE
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                } finally {
+                    image?.close()
+                }
+            }
+
+            reader.setOnImageAvailableListener(readerListener, handler)
+            val captureListener = object : CameraCaptureSession.CaptureCallback() {
+                override fun onCaptureCompleted(session: CameraCaptureSession, request: CaptureRequest, result: TotalCaptureResult) {
+                    super.onCaptureCompleted(session, request, result)
+                    startPreview()
+                }
+            }
+
+            cameraDevice.createCaptureSession(outputSurfaces, object : CameraCaptureSession.StateCallback() {
+                override fun onConfigured(session: CameraCaptureSession) {
+                    try {
+                        session.capture(captureBuilder.build(), captureListener, handler)
+                    } catch (e: CameraAccessException) {
+                        e.printStackTrace()
+                    }
+                }
+
+                override fun onConfigureFailed(session: CameraCaptureSession) {}
+            }, handler)
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+    }
+    private fun startPreview() {
+        if (cameraDevice == null || !textureView.isAvailable) return
+
+        val texture = textureView.surfaceTexture
+        texture!!.setDefaultBufferSize(textureView.width, textureView.height)
+        val surface = Surface(texture)
+        try {
+            captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+            captureRequestBuilder.addTarget(surface)
+            cameraDevice.createCaptureSession(listOf(surface), object : CameraCaptureSession.StateCallback() {
+                override fun onConfigured(session: CameraCaptureSession) {
+                    captureSession = session
+                    captureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO)
+                    try {
+                        captureSession.setRepeatingRequest(captureRequestBuilder.build(), null, handler)
+                    } catch (e: CameraAccessException) {
+                        e.printStackTrace()
+                    }
+                }
+
+                override fun onConfigureFailed(session: CameraCaptureSession) {}
+            }, handler)
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun save(bytes: ByteArray, file: File) {
+        var output: OutputStream? = null
+        try {
+            output = FileOutputStream(file)
+            output.write(bytes)
+        } finally {
+            output?.close()
+        }
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera()
+            } else {
+                Toast.makeText(requireContext(), "Camera permission denied", Toast.LENGTH_LONG).show()
+            }
+        } else if (requestCode == REQUEST_WRITE_PERMISSION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startCountdown()
+            } else {
+                Toast.makeText(requireContext(), "Write permission denied", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-    private fun fetchUserProfile() {
 
-        apiService.getUserProfile().enqueue(object : Callback<NameResponse> {
-            override fun onResponse(call: Call<NameResponse>, response: Response<NameResponse>) {
-                if (response.isSuccessful) {
-                    val userProfile = response.body()
-                    if (userProfile != null) {
-                        Log.d("AnotherFragment", "User Profile: ${userProfile.data}")
-                        binding.text.text = "${userProfile.data.name}"
-                    }
-                } else {
-                    Log.e("AnotherFragment", "Request failed with status: ${response.code()}")
-                }
-            }
-
-            override fun onFailure(call: Call<NameResponse>, t: Throwable) {
-                Log.e("AnotherFragment", "Network error: $t")
-                Toast.makeText(requireContext(), "Network error: $t", Toast.LENGTH_LONG).show()
-            }
-        })
-    }
 }
